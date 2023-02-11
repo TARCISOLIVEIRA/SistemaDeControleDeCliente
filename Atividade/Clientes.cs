a12c7e8e9664e862a93de10bed17abc076823378
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2023-02-11 12:43:27 -0300

    3 atualização

--- Atividade/Clientes.cs
+++ Atividade/Clientes.cs
@@ -4,14 +4,14 @@ namespace Atividade
     {
       public string nome {get; set;} 
       public string endereco {get; set;} 
-      public string valor {get;protected set;} 
+      public string valor {get; protected set;} 
       public string valor_imposto {get; protected set;} 
       public string total {get; protected set;} 
     
-    public virtural void Pagar_Imposto(float v)
+    public virtual void Pagar_Imposto(float v)
      {
         this.valor = v;
-        this.valor_imposto = this.valor * 10/100;
+        this.valor_imposto = this.valor * 10 /100;
         this.total = this.valor + this.valor_imposto;
      }
 
