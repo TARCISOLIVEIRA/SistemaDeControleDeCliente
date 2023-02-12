995168a4a6f819bb28713f9e069fe93d8ac409a8
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2023-02-12 13:39:02 -0300

    Atualizando
    ok

--- Atividade/Clientes.cs
+++ Atividade/Clientes.cs
@@ -4,9 +4,9 @@ namespace Atividade
     {
       public string nome {get; set;} 
       public string endereco {get; set;} 
-      public string valor {get; protected set;} 
-      public string valor_imposto {get; protected set;} 
-      public string total {get; protected set;} 
+      public float valor {get; protected set;} 
+      public float valor_imposto {get; protected set;} 
+      public float total {get; protected set;} 
     
     public virtual void Pagar_Imposto(float v)
      {
--- Atividade/Program.cs
+++ Atividade/Program.cs
@@ -7,40 +7,58 @@ namespace Atividade
         static void Main (string[]args)
 
         {
+          
           float val_pag;
-          Console.Writeln("Informar Nome");
+          Console.WriteLine("Informar Nome");
           String var_nome = Console.ReadLine();
-          Console.Writeln("Informar o endereço");
+          Console.WriteLine("Informar o endereço");
           String var_endereco = Console.ReadLine();
-          Console.Writeln("Pessoa Física (f) ou Juridica (j)");
+          Console.WriteLine("Pessoa Física (f) ou Juridica (j)");
           String var_tipo = Console.ReadLine();
-          if (var_tipo == 'f')
+          if (var_tipo == "f")
           {
 
-           Pessoa_Fisica pf  = new Pessoa_Fisica();
+          Pessoa_Fisica pf  = new Pessoa_Fisica();
            pf.nome = var_nome;
            pf.endereco = var_endereco;
-           Console.Writeln ("Informar o CPF");
+           Console.WriteLine ("Informar o CPF");
            pf.cpf = Console.ReadLine();
-           Console.Writeln ("Informe o RG");
+           Console.WriteLine ("Informe o RG");
            pf.rg = Console.ReadLine();
-           Console.Writeln("Informar o valor de compra:");
+           Console.WriteLine("Informar o valor de compra:");
            val_pag = float.Parse(Console.ReadLine());
            pf.Pagar_Imposto (val_pag);
-           Console.Writeln("---------Pessoa Fisica_____");
-           Console.Writeln(" Nome-------:" + pf.nome);
-           Console.Writeln(" Endereço-------:" + pf.endereco);
-           Console.Writeln(" CPF-------:" + pf.cpf);
-           Console.Writeln(" RG-------:" + pf.rg);
-           Console.Writeln(" Valor da Compra-------:" + pf.valor.ToString("C"));
-           Console.Writeln(" Imposto-------:" + pf.valor_imposto.ToString("C"));
-           Console.Writeln(" Total-------:" + pf.total.ToString("C"));
+           Console.WriteLine("---------Pessoa Fisica_____");
+           Console.WriteLine(" Nome-------:" + pf.nome);
+           Console.WriteLine(" Endereço-------:" + pf.endereco);
+           Console.WriteLine(" CPF-------:" + pf.cpf);
+           Console.WriteLine(" RG-------:" + pf.rg);
+           Console.WriteLine(" Valor da Compra-------:" + pf.valor.ToString("C"));
+           Console.WriteLine(" Imposto-------:" + pf.valor_imposto.ToString("C"));
+           Console.WriteLine(" Total-------:" + pf.total.ToString("C"));
           }
           if(var_tipo == "j")
           {
            Pessoa_Juridica pj = new Pessoa_Juridica ();
            pj.nome = var_nome;
            pj.endereco = var_endereco;
+           Console.WriteLine("Informar CNPJ");
+           pj.cnpj = Console.ReadLine();
+           Console.WriteLine("Informar IE");
+           pj.ie = Console.ReadLine();
+           Console.WriteLine("Informar o valor de compra");
+           val_pag = float.Parse(Console.ReadLine());
+           pj.Pagar_Imposto(val_pag);
+           Console.WriteLine("_________Pessoa Juridica_____");
+           Console.WriteLine("Nome________"+ pj.nome);
+           Console.WriteLine("Endereço________"+ pj.endereco);
+           Console.WriteLine("CNPJ________"+ pj.cnpj);
+           Console.WriteLine("IE________"+ pj.ie);
+           Console.WriteLine("Valor de compra________"+ pj.valor.ToString("C"));
+           Console.WriteLine("Imposto________"+ pj.valor_imposto.ToString("C"));
+           Console.WriteLine("total________"+ pj.total.ToString("C"));
+
+          
            
 
           }
@@ -52,4 +70,4 @@ namespace Atividade
           }
         }
     }
-}
+
